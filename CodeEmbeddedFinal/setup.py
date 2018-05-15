ba5fbe39cb859b98afd0fca440ee404253d0ecef
N03196784 <35784030+N03196784@users.noreply.github.com>
2018-05-14 21:46:38 -0400

    Update setup.py

--- CodeEmbeddedFinal/setup.py
+++ CodeEmbeddedFinal/setup.py
@@ -5,6 +5,6 @@ os.system("sudo apt-get install python-pip")
 os.system("sudo pip install gspread")
 os.system("sudo pip install --upgrade oauth2client")
 os.system("sudo pip install PyOpenSSL")
-os.system("crontab -l | { cat; echo '@reboot bash ~/Desktop/ELSpring2018/CodeEmbeddedFinal/getReading.sh'; } | crontab -")
+os.system("crontab -l | { cat; echo '@reboot bash /home/pi/CodeEmbeddedFinal/getReading.sh'; } | crontab -")
 os.system("python sheetReader.py")
 
